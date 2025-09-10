39f8b84be1177caedd09d0a44e3c9c4d27cfb0c6
Topech <benjaminlongbottom98@gmail.com>
2025-09-10 22:19:09 +0800

    tmp: move to other computer

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,68 @@
 class_name Player extends Node2D
 
 
+enum states {
+	INVALID = 0,
+	IDLE,
+	WALKING,
+	SUCKING,
+	ROLLING,
+	TWEEZED,
+}
+
+enum SpecialStateTransistions {
+	INVALID = 0,
+	ANY,
+	NONE
+}
+
+var transitions = {
+	states.INVALID: SpecialStateTransistions.INVALID,
+	states.IDLE: SpecialStateTransistions.ANY,
+	states.WALKING: SpecialStateTransistions.ANY,
+	states.SUCKING: SpecialStateTransistions.ANY,
+	states.ROLLING: [states.IDLE, states.WALKING],
+	states.TWEEZED: SpecialStateTransistions.NONE,
+}
+
+
+func validate_state_machine(states_: Dictionary, transitions_: Dictionary) -> Error:
+	var all_states = states_.values()
+	var all_states_with_transitions = transitions.keys()
+	
+	# check transition has all states
+	for state in all_states:
+		if not all_states_with_transitions.has(state):
+			return ERR_INVALID_DATA
+	
+	# check states has all transition states
+	for state in all_states_with_transitions:
+		if not all_states.has(state):
+			return ERR_INVALID_DATA
+		
+	return OK
+
+
+func check_state_transition(current: states, next: states) -> Error:
+	if not transitions.has(current):
+		return FAILED
+	if transitions[current] == SpecialStateTransistions.NONE:
+		return FAILED
+	if transitions[current] == SpecialStateTransistions.INVALID:
+		return FAILED
+	if transitions[current] == SpecialStateTransistions.ANY:
+		return OK
+	if next in transitions[current]:
+		return OK
+	return FAILED
+
+
 # contoller support
 @export var player_id := Global.players.NO_PLAYER
 @onready var player_device: PlayerDevice = PlayerInputDevices.get_players_device(player_id)
 
 # data structures
+@onready var state = states.IDLE
 @onready var metrics = PlayerMetrics.new()
 
 # control nodes
@@ -24,6 +81,7 @@ class_name Player extends Node2D
 
 
 func _process(delta: float) -> void:
+	# ! DEBUG ONLY
 	if (Input.is_key_pressed(KEY_P)):
 		queue_free()
 	
@@ -34,21 +92,19 @@ func _process(delta: float) -> void:
 	roll_effect.roll_speed = 3 * metrics.speed
 
 	if tweeze_event.triggered:
-			walk_effect.enabled = false
-			roll_effect.enabled = false
-			suck_effect.enabled = false
-			return
+		state = states.TWEEZED
 
 	# process inputs
-	if (walk_event.triggered and not suck_effect.is_activated):
-			walk_effect.activate()
-	else:
-			walk_effect.deactivate()
+	if (walk_event.triggered and state != states.SUCKING):
+			state = states.WALKING
+			#walk_effect.activate()
+	#else:
+			#walk_effect.deactivate()
 
 	if (
 			roll_event.triggered
-			and not roll_effect.is_activated
-			and not suck_effect.is_activated
+			and state != states.ROLLING
+			and state != states.SUCKING
 	):
 			roll_effect.activate()
 			await roll_effect.deactivated
